e347e9024bb3bcc3b2c1492acca7d83680394f9c
Chirag Chadha <chiragkchadha@gmail.com>
2023-06-29 14:47:12 -0400

    fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,11 +2,11 @@ function Hero() {
   return (
     <div className="bg-white flex flex-row justify-center items-center min-h-[600px]">
       <div className="flex flex-row items-start gap-6">
-        <div className="flex flex-col justify-center items center">
+        <div className="flex flex-col justify-center items center mt-[3.75rem]">
           <h3 className="stroke !text-5xl">forge your own</h3>
         </div>
         <div className="flex flex-col justify-center items center gap-6 slide">
-          <h3 className="text-forge-orange !text-5xl slide-up">wumbo</h3>
+          <h3 className="text-forge-orange !text-5xl slide-up">journey</h3>
           <h3 className="text-forge-orange !text-5xl slide-up">path</h3>
           <h3 className="text-forge-orange !text-5xl slide-up">experience</h3>
         </div>
