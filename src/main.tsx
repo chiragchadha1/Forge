74f606b60b639e86b74ab27f14ae1391152e8119
Chirag Chadha <chiragkchadha@gmail.com>
2023-06-29 15:06:05 -0400

    Added quote section

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import NavBar from './components/NavBar.tsx'
 import Hero from './components/Hero.tsx'
-import './css/index.css'
+import './assets/css/index.css'
+import Quote from './components/Quote.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <NavBar />
     <Hero />
+    <Quote />
   </React.StrictMode>,
 )
